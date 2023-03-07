19083ffb86329a5ba8373f93b02f46d3c1034e7c
Taylor Geisse <1383010+tgeisse@users.noreply.github.com>
2023-03-07 09:46:38 -0800

    Initial Commit

---
+++ Simple Sudoku/Simple_SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Simple_SudokuApp.swift
+//  Simple Sudoku
+//
+//  Created by Taylor Geisse on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct Simple_SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
